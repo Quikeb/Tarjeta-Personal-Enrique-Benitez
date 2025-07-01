2c3fa8009fbe2e482bba0ae7d5a9a122c1e49c44
Quikeb <182255399+Quikeb@users.noreply.github.com>
2025-07-01 11:01:19 -0600

    cambio dinamico de icon theme y marca grafica

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
-/* Enlazar los elementos del html */
-const togglebutton = document.getElementById ("toggle-button");
+/* enlazar los elementos de HTML */
+
+const toggleButton = document.getElementById("toggle-button");
 const body = document.body;
+const logo = document.getElementById("logo");
 
+/* función para ejecutar un código */
 
+toggleButton.addEventListener("click", () => {
+  body.classList.toggle("dark");
 
-/*  Creacion de una funcion para ejecutar un codigo  */
+  // cambiamos el logo segun el tema
 
+  const isDark = body.classList.contains("dark"); // true - false
 
-togglebutton.addEventListener("click",() => {
-  body.classlist.toggle ("dark")
-});
+  logo.src = isDark ?  "./assets/marca-grafica-dark.svg" : "./assets/marca-grafica.svg"
 
+} )
